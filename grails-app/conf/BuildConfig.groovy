15c36be7a63c0d262cefaefe331925987d1a9318
Ali Tanwir <ali.tanwir@nexthoughts.com>
2016-09-24 11:01:51 +0530

    Added plugin twitter-bootstrap:3.3.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -74,6 +74,8 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
         runtime ":jquery-ui:1.10.4"
         compile "org.grails.plugins:spring-security-core:2.0.0"
+        runtime ':twitter-bootstrap:3.3.4'
+
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
         //compile ":less-asset-pipeline:1.10.0"
